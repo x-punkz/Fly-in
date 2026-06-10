70b5e1d55f9c552d647a938e698d5b81a84c0e15
daniviei <r2sskateboard@gmail.com>
2026-06-09 21:12:49 -0300

    o pessoal é chato

--- gui.py
+++ gui.py
@@ -10,9 +10,18 @@ class App:
         pygame.display.set_caption("Fly_in")
         icon = pygame.image.load("images/icon.png")
         pygame.display.set_icon(icon)
-        # self.bg = pygame.image.load("") #  escolher a imagem e por o caminho aqui
+        self.bg = pygame.image.load("images/mapa.png")  # escolher a imagem e por o caminho aqui
+        # pygame.mixer.music.load() #  da p por musica
         self.width, self.height = 1000, 800
-        self.window = pygame.display.set_mode((self.width, self.height))
+        self.window = pygame.display.set_mode((self.width, self.height),
+                                              pygame.RESIZABLE)
+        self.virtual_window = pygame.Surface((self.width, self.height))
+        self.game = pygame.Surface((self.width * 3/4, self.height))
+        self.menu = pygame.Surface((self.width * 1/4, self.height))
+        game_color = (255, 255, 255)
+        self.menu.fill(game_color)
+        menu_color = (128, 128, 128)
+        self.menu.fill(menu_color)
 
     @staticmethod
     def parse_file() -> Map:
@@ -36,9 +45,21 @@ class App:
     def run(self) -> None:
         running: bool = True
         while (running):
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            self.virtual_window.blit(self.game, (0, 0))
+            self.virtual_window.blit(self.menu, (self.game.get_width(), 0))
+
+            height = self.window.get_height()
+            width = self.window.get_width()
+            change_size = pygame.transform.scale(self.virtual_window,
+                                                 (width, height))
+            self.window.blit(change_size)
+            self.game.blit(self.bg)
+
             pygame.display.flip()
         pygame.quit()
 
